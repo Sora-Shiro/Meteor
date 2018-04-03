3b90da9cf0e2f61aa42053b10bc70b9ac2ed7514
SoraShiro <sora95shiro@gmail.com>
2018-04-04 01:59:27 +0800

    Basic code v0.0.1.1 - opacity anim

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,50 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int bottomIndex = 1;
+class _MyHomePageState extends State<MyHomePage>
+    with TickerProviderStateMixin {
+  int _currentIndex = 1;
+  int _beforeIndex = -1;
+  AnimationController _animController;
+  Animation _curve;
+  Animation _tweenAlpha;
+  AnimationStatus _animStatus;
+
+  @override
+  void initState() {
+    super.initState();
+    _animController = new AnimationController(
+        duration: const Duration(milliseconds: 300), vsync: this);
+    _curve = new CurvedAnimation(parent: _animController, curve: Curves.easeIn);
+    _tweenAlpha = new Tween(begin: 0.0, end: 1.0).animate(_curve)
+      ..addListener(() {
+        setState(() {
+
+        });
+      })
+      ..addStatusListener((status) {
+        setState(() {
+          _animStatus = status;
+        });
+      });
+    _animController.forward();
+  }
+
+  dispose() {
+    _animController.dispose();
+    super.dispose();
+  }
+
+  double calculateOpacity(index) {
+    print(_tweenAlpha.value);
+    if (_currentIndex == index && _beforeIndex != index) {
+      return _curve.value;
+    } else if (_currentIndex != index && _beforeIndex == index) {
+      return 1.0 - _curve.value;
+    }
+    return 0.0;
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -40,25 +82,34 @@ class _MyHomePageState extends State<MyHomePage> {
           child: new Stack(
             children: <Widget>[
               new Offstage(
-                offstage: bottomIndex != 0,
-                child: new TickerMode(
-                  enabled: bottomIndex == 0,
-                  child: new MaterialApp(home: new LeftPage()),
-                ),
+                offstage: _animStatus == AnimationStatus.completed &&
+                    _currentIndex != 0,
+                child: new Opacity(
+                    opacity: calculateOpacity(0),
+                    child: new TickerMode(
+                      enabled: _currentIndex == 0,
+                      child: new MaterialApp(home: new LeftPage()),
+                    )),
               ),
               new Offstage(
-                offstage: bottomIndex != 1,
-                child: new TickerMode(
-                  enabled: bottomIndex == 1,
-                  child: new MaterialApp(home: new MidPage()),
-                ),
+                offstage: _animStatus == AnimationStatus.completed &&
+                    _currentIndex != 1,
+                child: new Opacity(
+                    opacity: calculateOpacity(1),
+                    child: new TickerMode(
+                      enabled: _currentIndex == 1,
+                      child: new MaterialApp(home: new MidPage()),
+                    )),
               ),
               new Offstage(
-                offstage: bottomIndex != 2,
-                child: new TickerMode(
-                  enabled: bottomIndex == 2,
-                  child: new MaterialApp(home: new RightPage()),
-                ),
+                offstage: _animStatus == AnimationStatus.completed &&
+                    _currentIndex != 2,
+                child: new Opacity(
+                    opacity: calculateOpacity(2),
+                    child: new TickerMode(
+                      enabled: _currentIndex == 2,
+                      child: new MaterialApp(home: new RightPage()),
+                    )),
               ),
             ],
           ),
@@ -70,7 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
             });
           },
           child: new BottomNavigationBar(
-            currentIndex: bottomIndex,
+            currentIndex: _currentIndex,
             items: [
               new BottomNavigationBarItem(
                 icon: const Icon(Icons.star),
@@ -86,9 +137,12 @@ class _MyHomePageState extends State<MyHomePage> {
               )
             ],
             onTap: (int index) {
-              print(index);
               setState(() {
-                this.bottomIndex = index;
+                if (index != this._currentIndex) {
+                  this._beforeIndex = this._currentIndex;
+                  this._currentIndex = index;
+                  _animController.forward(from: 0.0);
+                }
               });
             },
           ),
